05ac61f91595104fa42e5f8695d1bde09b6c9b03
tintinve <pedroluismejia@gmail.com>
2018-10-25 12:05:51 +0200

    starting radio buttons

--- js/aScript.js
+++ js/aScript.js
@@ -1,10 +1,17 @@
-/* ASK JONAS WHYT PARCEL DOES NOT ALLOWS TO FETCH FROM A LOCAL JSON FILE AND LIVE-SERVER DOES
-const template = document.querySelector("#template1").content;
-const main = document.querySelector("main");
-fetch("bigArticles.json")
+/*const template = document.querySelector("template").content;
+const section = document.querySelector("section");
+const fetchLink = "mainPromos.json";
+fetch(fetchLink)
   .then(res => res.json())
   .then(data => show(data));
 
 function show(data) {
-  console.log(data);
+  data.forEach(render);
+}
+
+function render(aPromo) {
+  let clone = template.cloneNode(true);
+  console.log(aPromo.uno);
+  clone.querySelector("h2").textContent = aPromo.uno;
+  section.appendChild(clone);
 }*/
