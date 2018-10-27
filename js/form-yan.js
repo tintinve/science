8e1651041191acd3df0a5259f0923f2ee2ce6a2a
liyan <yanx0167@stud.kea.dk>
2018-10-27 11:11:28 +0200

    Refined: validation trigger and hints. Added: only allow submit when all input fields passed validations

--- js/form-yan.js
+++ js/form-yan.js
@@ -1,13 +1,21 @@
 "use strict";
 const form = document.querySelector("form");
 const inputFieldS = document.querySelectorAll("input");
+const submitButton = document.querySelector('button[type="submit"]');
 inputFieldS.forEach(checkInput);
 function checkInput(input, i) {
-  input.addEventListener("blur", () => {
+  //check validity of the input flied that loses focus
+  input.addEventListener("input", () => {
     if (form.elements[i].validity.valid) {
-      input.classList.add("true");
+      input.className = "true";
     } else {
-      input.classList.add("false");
+      input.className = "false";
+    }
+    // check how many input field has passed validity check, when all passed then allow submit button
+    let validCount = document.querySelectorAll(".true").length;
+    console.log(validCount);
+    if (validCount === inputFieldS.length) {
+      submitButton.classList.remove("not-active");
     }
   });
 }
