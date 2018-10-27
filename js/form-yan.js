325ad4aa93ace7ec5c62d3c1c7ca1c19b8bdb9b5
liyan <yanx0167@stud.kea.dk>
2018-10-27 10:29:18 +0200

    added: when moving to next input field, check validity and give hint if not valid

--- js/form-yan.js
+++ js/form-yan.js
@@ -1,1 +1,13 @@
 "use strict";
+const form = document.querySelector("form");
+const inputFieldS = document.querySelectorAll("input");
+inputFieldS.forEach(checkInput);
+function checkInput(input, i) {
+  input.addEventListener("blur", () => {
+    if (form.elements[i].validity.valid) {
+      input.classList.add("true");
+    } else {
+      input.classList.add("false");
+    }
+  });
+}
