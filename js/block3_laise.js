8690cb43c542d450b90b023d72c225dcac57fa9a
LaiseBang <lbangh@yahoo.com>
2018-10-30 08:58:49 +0100

    styling on block3 is done and hide radio buttons

---
+++ js/block3_laise.js
@@ -0,0 +1,36 @@
+/* "use strict";
+
+
+
+// when one of the radiobutton/divs have been changed addEventlistener("change")
+// Clear any border color on the radiobuttons/divs
+// find out which div has been changed
+// change bordercolor on the div that has changed
+// If radiobutton changes again remove border on previus and set new (loop)
+
+
+
+
+let selected = document.querySelectorAll(".radio_button");
+selected.addEventListener("click", function() {
+  this.style.border = "5px solid red";
+}); */
+
+/* addEventListener("click", addBorder);
+
+function addBorder() {
+  document.querySelector(".radio_button").style.border = "5px solid red";
+}
+ */
+
+//var div = document.getElementById( 'div_id' );
+/* div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+  var h2s = this.getElementsByTagName( 'h2' );
+  h2s[0].style.backgroundColor = 'blue';
+}; */
+/* 
+if (document.getElementById("2issues").checked) {
+    style.border = "5px solid red";
+}
+console.log(this); */
