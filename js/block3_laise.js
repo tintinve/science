7a258b4493af18370563934de2fddd2a3f7b25d0
xiaobaoxixi <yanli.xiaobaoxixi@gmail.com>
2018-10-30 19:16:16 +0100

    fixed CSS conflicts. Changed: JS linked to new selectors etc.

--- js/block3_laise.js
+++ js/block3_laise.js
@@ -4,11 +4,14 @@
 const allsubscriptionBox = document.querySelectorAll(
   '.radio_button input[type="radio"]'
 );
+const issueP = document.querySelector(".block6 .issue-choice");
+
 allsubscriptionBox.forEach(p =>
   p.addEventListener("click", () => {
     // remove previously checked
     allsubscriptionBox.forEach(p => {
       p.parentElement.style.border = "none";
+      p.removeAttribute("checked");
     });
     // add newly checked
     document
@@ -22,5 +25,28 @@ allsubscriptionBox.forEach(p =>
     document.querySelector(
       'input[name="nr-of-issues"]:checked'
     ).nextElementSibling.nextElementSibling.style.display = "inherit";
+    issueP.textContent = "";
+    issueP.textContent += p.nextElementSibling.textContent + " magazines ";
+    issueP.textContent +=
+      p.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent;
+    let sumStringWithComma;
+    let originalText =
+      p.nextElementSibling.nextElementSibling.nextElementSibling
+        .nextElementSibling.textContent;
+    if (originalText.indexOf("for") > -1) {
+      let priceString = originalText.substring(
+        originalText.indexOf("for") + 4,
+        originalText.indexOf("kr")
+      );
+      let priceNr = Number(priceString.replace(",", ""));
+      let sum = priceNr + 3950; // porto
+      let sumString = String(sum);
+      sumStringWithComma =
+        sumString.substring(0, sumString.length - 2) +
+        "," +
+        sumString.substring(sumString.length - 2);
+      document.querySelector(".i-alt").textContent =
+        "I Alt: " + sumStringWithComma + "kr.";
+    }
   })
 );
